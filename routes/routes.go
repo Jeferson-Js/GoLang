f4023e08cab61600e9af7129577b0f7edc5dde0d
Your Name3 <you@example.com>
2023-01-31 14:59:45 -0300

    Atualização na linha de comando.

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", controllers.Home).Methods("Get")
+	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades/", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornUmaPersonalidade).Methods("Get")
 	log.Fatal(http.ListenAndServe(":8000", r))
